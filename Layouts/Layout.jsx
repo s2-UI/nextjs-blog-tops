ec182b14b904a5522f4fa5ed7b44e921c23b0e02
s2 <samueldf83@gmail.com>
2023-02-24 22:57:02 +0100

    *

--- Layouts/Layout.jsx
+++ Layouts/Layout.jsx
@@ -11,17 +11,20 @@ function Layout({ children, title, description }) {
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="keywords" content="televisores 2023, comparativa televisores, mejores televisores, guía de compra de televisores, marcas de televisores, modelos de televisores, resolución de televisores, tamaño de televisores, pantallas OLED, pantallas QLED, HDR en televisores, Smart TV, precios de televisores, opiniones de televisores, análisis de televisores" />
+        <meta name="author" content="Samuel" />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="favicon.png" />
       </Head>
 
       <main className={styles.Main}>
         <header className={styles.Header}>
             <div className={styles.Header_container}>
                 <div className={styles.Header_left}>
-                    <Image className={styles.Logo} src="/next.svg" alt="" width={100} height={100} />
+                    <Image className={styles.Logo} src="/logo.png" alt="Logo principal de pickiwise" width={512} height={512} />
                 </div>
                 <div className={styles.Header_right}>
-                    <nav className={styles.Nav}>
+                    {/* <nav className={styles.Nav}>
                         <ul className={styles.Nav_ul}>
                             <li className={styles.Nav_li}>
                                 <Link className={styles.Nav_a} href="" title="">
@@ -39,7 +42,7 @@ function Layout({ children, title, description }) {
                                 </Link>
                             </li>
                         </ul>
-                    </nav>
+                    </nav> */}
                 </div>
             </div>
         </header>
