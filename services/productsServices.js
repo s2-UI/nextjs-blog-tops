2fb63f2d35d0223714076f72b451d1efc1917c20
s2 <samueldf83@gmail.com>
2023-02-24 14:52:17 +0100

    test fetch

--- services/productsServices.js
+++ services/productsServices.js
@@ -1,5 +1,5 @@
 export async function getProducts(){
-    const request = await fetch ('http://localhost:3000/api/televisions')
+    const request = await fetch ('https://tops-six.vercel.app/api/televisions')
     const data = await request.json();
 
     return data;
