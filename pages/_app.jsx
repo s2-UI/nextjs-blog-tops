1004ae6002708903c9bd29896998911900b2d086
s2 <samueldf83@gmail.com>
2023-02-28 23:54:24 +0100

    Analitics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,12 @@
+import { Analytics } from '@vercel/analytics/react';
+
 import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  ) 
 }
