c85b07b0ed243198d65a4b0d41081fb338437787
s2 <samueldf83@gmail.com>
2023-03-29 00:26:44 +0200

    *

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,15 @@
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react'
+import TagManager from 'react-gtm-module'
 
 import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
+  TagManager.initialize({ gtmId: 'G-N18YMBS4W7' })
+
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
     </>
-  ) 
+  )
 }
